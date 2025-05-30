ec43ae9df60c0be7f0e14a78e7e3fdbea75a8955
Merick-create <ric05bras@gmail.com>
2025-05-30 12:17:05 +0200

    category

---
+++ src/api/categories/category-entity.ts
@@ -0,0 +1,6 @@
+import { Types } from "mongoose"
+
+export type CategoryEntity={
+    id:Types.ObjectId;
+    name:string;
+}
--- src/api/routes.ts
+++ src/api/routes.ts
@@ -4,10 +4,12 @@ import userRouter from "./user/user.router";
 import productRouter from "./product/product-router";
 import cartRouter from "./cart/cart-router";
 import { isAuthenticated } from "../lib/auth/auth.middleware";
+import CategoryRouter from './categories/category-router';
 
 const router = Router();
 router.use('/users', userRouter, isAuthenticated);
 router.use(authRouter);
 router.use('/product',productRouter,isAuthenticated);
 router.use('/cart',cartRouter,isAuthenticated)
+router.use('category',CategoryRouter,isAuthenticated);
 export default router;
