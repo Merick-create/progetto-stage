b7ac16f5246fa5837df0e79b8e676f5045917249
tiziani <lorenzo.tiziani04@gmail.com>
2025-06-11 16:42:28 +0200

    pagina utente, modifica img

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import UserService from "./user.service";
+import { UserModel } from "./user.model";
+import { User } from "./user.entity";
 export const me = async (
     req: Request, 
     res: Response, 
@@ -19,5 +21,33 @@ export const filtered = async (req: Request, res: Response, next: NextFunction) 
 
         const statusCode = error.message === 'Invalid role' ? 400 : 500;
         res.status(statusCode).json({ error: error.message });
+    }
+};
+
+
+
+export const updateMe = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const userId = (req.user as User).id!;
+        const { picture } = req.body;
+
+        if (!userId || typeof picture !== 'string') {
+            res.status(400).json({ error: 'Invalid data' });
+        }
+
+        const updatedUser = await UserModel.findByIdAndUpdate(
+            userId,
+            { picture },
+            { new: true }
+        );
+
+        if (!updatedUser) {
+            res.status(404).json({ error: 'User not found' });
+        }
+
+        res.json(updatedUser);
+    } catch (error) {
+        console.error('Error updating user picture:', error);
+        res.status(500).json({ error: 'Server error' });
     }
 };
